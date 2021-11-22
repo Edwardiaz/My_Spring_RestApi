fbca7ef950e1938dc190b68d40200a1317578533
Jorge Diaz <jorgediaz9515@gmail.com>
2021-11-22 09:40:43 -0600

    1° commit
    Carga del proyecto base.

---
+++ src/main/java/com/jd/restApi/JdRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.jd.restApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdRestApiApplication.class, args);
+	}
+
+}
